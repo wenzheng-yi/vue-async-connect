dbca7f527c6af39b20378deae57c3018a97affc2
廖政 <813144314@qq.com>
2022-03-09 14:36:00 +0800

    fix(useBatchDefine): improve type

--- src/useBatchDefine.ts
+++ src/useBatchDefine.ts
@@ -1,14 +1,16 @@
 
-type ImportObj = Record<string, () => Promise<{
+type ImportObj = Record<string | number | symbol, () => Promise<{
   [key: string]: any;
 }>>
-function useBatchDefine(importObj: ImportObj) {
+function useBatchDefine<T extends string | number | symbol>(importObj: ImportObj) {
   let result = {} as ImportObj
   for (let key in importObj) {
     let name = key.split('/').slice(-2).join('/')
     name = name.replace(/\.[^.]*$/, '')
     result[name] = importObj[key]
   }
-  return result
+  return result as Record<T, () => Promise<{
+    [key: string]: any;
+  }>>
 }
 export default useBatchDefine
